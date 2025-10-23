fd58bfad6cdc119d8812147cfcb3545ec6537010
nazirafiff28 <nazirafiff097@gmail.com>
2025-10-23 11:05:59 +0700

    Update tugas.py

--- tugas.py
+++ tugas.py
@@ -1,15 +1,10 @@
-# Program Penggajian PT. DINGIN DAMAI
-
-# Gaji pokok
 gaji_pokok = 300000
 
-# Input data karyawan
 nama = input("Masukkan nama karyawan        : ")
 golongan = int(input("Masukkan golongan (1/2/3)    : "))
 pendidikan = input("Masukkan pendidikan (SMA/D1/D3/S1): ")
 jam_kerja = int(input("Masukkan jumlah jam kerja    : "))
 
-# --- Hitung Tunjangan Jabatan ---
 if golongan == 1:
     tunjangan_jabatan = 0.05 * gaji_pokok
 elif golongan == 2:
@@ -19,7 +14,6 @@ elif golongan == 3:
 else:
     tunjangan_jabatan = 0
 
-# --- Hitung Tunjangan Pendidikan ---
 if pendidikan.upper() == "SMA":
     tunjangan_pendidikan = 0.025 * gaji_pokok
 elif pendidikan.upper() == "D1":
@@ -31,16 +25,13 @@ elif pendidikan.upper() == "S1":
 else:
     tunjangan_pendidikan = 0
 
-# --- Hitung Honor Lembur ---
 if jam_kerja > 8:
     lembur = (jam_kerja - 8) * 3500
 else:
     lembur = 0
-
-# --- Total Gaji ---
+    
 total_gaji = gaji_pokok + tunjangan_jabatan + tunjangan_pendidikan + lembur
 
-# --- Output ---
 print("\n=== SLIP GAJI KARYAWAN ===")
 print("Nama Karyawan        :", nama)
 print("Golongan             :", golongan)
@@ -52,3 +43,4 @@ print("Tunjangan Pendidikan : Rp", int(tunjangan_pendidikan))
 print("Honor Lembur         : Rp", int(lembur))
 print("-------------------------------")
 print("Total Gaji           : Rp", int(total_gaji))
+
